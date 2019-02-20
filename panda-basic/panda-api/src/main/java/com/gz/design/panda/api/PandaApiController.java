dc44bd3fbc5b4dfc2b3b6b1b8a3f37e0c9af82bc
gaozhan <gaozhan@yofish.com>
2019-02-20 16:53:38 +0800

    修改项目结构

---
+++ panda-basic/panda-api/src/main/java/com/gz/design/panda/api/PandaApiController.java
@@ -0,0 +1,19 @@
+package com.gz.design.panda.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+/**
+ * @Auther: Gz.
+ * @Date: 2019/2/20 14:45
+ * @Description:
+ */
+@SpringBootApplication
+public class PandaApiController {
+
+  public static void main(String[] args) {
+    SpringApplication.run(PandaApiController.class,args);
+  }
+
+}
