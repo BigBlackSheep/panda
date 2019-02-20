b484c266151a11d911475f9f30e18ca8cca57df0
gaozhan <gaozhan@yofish.com>
2019-02-20 17:53:26 +0800

    update

--- panda-basic/panda-biz/src/main/java/com/gz/design/panda/biz/utils/EmaiUtil.java
+++ panda-basic/panda-biz/src/main/java/com/gz/design/panda/biz/utils/EmaiUtil.java
@@ -39,4 +39,26 @@ public class EmaiUtil {
 
     }
 
+    public NewResponseUtil sendEmail(String fromAddress, String[] toAddress, String title, String content) {
+
+        try {
+            //建立邮件消息
+            SimpleMailMessage mainMessage = new SimpleMailMessage();
+            //发送者
+            mainMessage.setFrom(fromAddress);
+            //接收者
+            mainMessage.setTo(toAddress);
+            //发送的标题
+            mainMessage.setSubject(title);
+            //发送的内容
+            mainMessage.setText(content);
+            mailSender.send(mainMessage);
+        } catch (Exception e) {
+            log.error("发送简单邮件失败", e);
+            return NewResponseUtil.newFailureResponse("发送失败");
+        }
+        return NewResponseUtil.newSucceedResponse("发送成功");
+
+    }
+
 }
